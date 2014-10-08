f6cbf45bd5ce4dfa3f1111367fbe597dce815fbb
Jamey Hicks <jamey.hicks@qrclab.com>
2014-10-08 17:09:13 -0400

    allow specification of architectures, branch and revision for projects

--- xbsv-master/projects.py
+++ xbsv-master/projects.py
@@ -2,6 +2,7 @@
 import buildbot
 import jinja2
 import json
+import re
 import subprocess
 import sys
 import twisted
@@ -34,14 +35,31 @@ class AddProjectResource(ActionResource):
 	projects = json.load(open('projects.json'))
         log.msg('loaded projects.json')
 
+	log.msg(str(req.args.items()))
         projectname = req.args.get('projectname', [])[0]
 	projectrepo = req.args.get('projectrepo', [])[0]
-	if projectname not in projects:
-	    projects[projectname] = {"repo": projectrepo}
+	boardaddr = req.args.get('boardaddr', [])[0]
+        log.msg(str(req.args.get('arches', [])))
+        arches = req.args.get('arches', ['bluesim'])
+        revision = req.args.get('revision', [])
+        branch = req.args.get('branch', [])
+
+        m = re.match('git://github.com/(.*)/(.*).git', projectrepo)
+	if projectname in projects:
+	    msg = 'project ' + projectname + ' already exists'
+        elif not m:
+            msg = 'repo "' + projectrepo + ' must be of the form git://github.com/username/project.git'
+	else:
+            p = {"repo": projectrepo}
+            if arches:
+                p['arches'] = arches
+            if revision:
+                p['revision'] = revision[0]
+            elif branch:
+                p['branch'] = branch[0]
+	    projects[projectname] = p
 	    json.dump(projects, open('projects.json', 'w'))
 	    subprocess.call(["/usr/bin/buildbot", "reconfig", "/scratch/buildbot/master"])
 	    msg = 'added project: ' + ' '.join(req.args.get('projectname', '')) + ' url: ' + ' '.join(req.args.get('projectrepo', ''))
-	else:
-	    msg = 'project ' + projectname + ' already exists'
         defer.returnValue((path_to_root(req), msg))
 
